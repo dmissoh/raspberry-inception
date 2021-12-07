5c8c184be7d427c5156a0d8ad147abf9932e3bd3
Dimitri Missoh <dmissoh@gmail.com>
2021-12-07 10:45:29 +0100

    test

--- label_image.py
+++ label_image.py
@@ -19,7 +19,7 @@ with tf.gfile.FastGFile("/tf_files/retrained_graph.pb", 'rb') as f:
 with tf.Session() as sess:
     # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    
+    # this is just test comment    
     predictions = sess.run(softmax_tensor, \
              {'DecodeJpeg/contents:0': image_data})
     
